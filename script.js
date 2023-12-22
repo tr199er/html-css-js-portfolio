1902a5ac721fca55a388e9d7787520e618092f09
akanni malik <akannimalik1@gmail.com>
2023-12-21 20:06:10 -0500

    sorting navbar

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 function toggleMenu(){
     const menu = document.querySelector(".menu-links");
     const icon = document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open")
-    icon.classList.toggle("open")
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
 }
+
+
+
+  
